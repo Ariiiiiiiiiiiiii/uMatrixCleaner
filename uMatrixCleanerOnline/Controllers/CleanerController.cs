20fa1c0067e0657d53779589c641adf4be97f983
gqqnbig <gqqnb2005@gmail.com>
2018-08-05 16:10:45 -0700

    Add sample code to write a file to file system

--- uMatrixCleanerOnline/Controllers/CleanerController.cs
+++ uMatrixCleanerOnline/Controllers/CleanerController.cs
@@ -30,6 +30,7 @@ namespace uMatrixCleanerOL.Controllers
         [HttpPost]
         public string Post([FromBody]string value)
         {
+            System.IO.File.WriteAllText("input.txt", value);
             return $"{value.Split('\r', '\n').Length} rules available.";
         }
 
