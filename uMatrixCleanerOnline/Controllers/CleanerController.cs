811f5ef86e4315b5752f40d89e5fdc7bed1a6a54
gqqnbig <gqqnb2005@gmail.com>
2018-08-05 14:23:41 -0700

    Add asp.net mvc core website that calls uMatrixCleaner

---
+++ uMatrixCleanerOnline/Controllers/CleanerController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace uMatrixCleanerOL.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Cleaner")]
+    public class CleanerController : Controller
+    {
+
+        // GET: api/Default
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Default/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Default
+        [HttpPost]
+        public string Post([FromBody]string value)
+        {
+            return $"{value.Split('\r', '\n').Length} rules available.";
+        }
+
+        // PUT: api/Default/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
